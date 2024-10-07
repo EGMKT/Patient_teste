a68b89b26b100b303f456da3d3bb0b92656258bb
EGMKT <adm@evergreenmkt.com.br>
2024-10-06 23:39:42 -0300

    Add files via upload

---
+++ build/Python_Build_Back/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import MedicoListCreateView, PacienteListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/medicos/', MedicoListCreateView.as_view(), name='medico_list_create'),
+    path('api/pacientes/', PacienteListCreateView.as_view(), name='paciente_list_create'),
+]
